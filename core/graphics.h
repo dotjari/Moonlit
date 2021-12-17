2a44f984ce155ede7f6ba940fc1877107deb50dd
João <82425115+MentisEt@users.noreply.github.com>
2021-12-16 22:16:16 -0300

    Update graphics.h

--- core/graphics.h
+++ core/graphics.h
@@ -1,5 +1,5 @@
-#ifndef DIALOG_H
-#define DIALOG_H
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
 #include "window.h"
 #include <windows.h>
 #include <windowsx.h>
@@ -9,7 +9,7 @@ struct sRectangle
 {
 
     HDC rectDC;
-    //Draw a rectangle - width, height, x, y, color (use teh RGB(r,g,b) function)
+    //Draw a rectangle - width, height, x, y, color (use the RGB(r,g,b) function)
     void Draw(int w, int h, int x, int y, int c)
     {
         HBRUSH hBrush = CreateSolidBrush(c); //Create the color for the Rectangle
@@ -21,4 +21,18 @@ struct sRectangle
         //For future use
     }
 };
+
+//Draw a pixel (isn't a literal pixel because the size is variable)
+//x, y, color (use the RGB(r,g,b), size function)
+void drawPixel(int x, int y, int c, int size = 1)
+{
+    HBRUSH hBrush = CreateSolidBrush(c); //Create a Brush to paint the "pixel"
+    RECT r; //positions
+    r.left = x;//x
+    r.top = y;//y
+    r.right = x + size;//width
+    r.bottom = y + size;//height
+    FillRect(memoryDC,&r,hBrush);//Paint the pixel
+    DeleteObject(hBrush);//Delete the Brush after using it
+}
 #endif
